c03921f73340b089de09ac9e8f7c8a88f9fa669e
Clacson <cookiellacson@gmail.com>
2017-01-11 07:10:21 +0000

    debugged and removed song.jsx

--- client/src/components/SongsList.jsx
+++ client/src/components/SongsList.jsx
@@ -1,27 +1,18 @@
 var React = require('react');
-var Song = require('./Song');
 
-var SongsList = React.createClass({
-
-  // getInitialState: function(){
-  //   return { selectedIndex: undefined}
-  // },
-
-  render: function(){
-
-    var mappedSongs = this.props.songs.map(function(song, index){
-      return <li
-      key={index}>
-      {song['im:name']['label']}</li>});
-
+var SongsList = function(props){
+  var mappedSongs = props.songs.map(function(song, index){
     return (
-      <ol id='songs'>
-      {mappedSongs}
-      </ol>
-      
-      );
-  }
+    <li key={index}>
+    {song['im:name']['label']}
+    </li>
+    )})
 
-});
+  return (
+    <ol id='songs'>
+    {mappedSongs}
+    </ol>
+    );
+}
 
 module.exports = SongsList;
